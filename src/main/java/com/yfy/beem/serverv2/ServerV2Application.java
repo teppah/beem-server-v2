85f4ffdce26e2c4791558fd4693667dfd4e47d7f
teppah <yifengyang01@gmail.com>
2018-12-25 21:21:07 -0500

    Initial IntelliJ commit

---
+++ src/main/java/com/yfy/beem/serverv2/ServerV2Application.java
@@ -0,0 +1,14 @@
+package com.yfy.beem.serverv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerV2Application.class, args);
+    }
+
+}
+
