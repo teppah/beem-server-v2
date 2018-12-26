1fc1aaed99bd8fbda0f38fa6aff7f0993dddbe37
teppah <yifengyang01@gmail.com>
2018-12-26 18:03:49 -0500

    removed localdatetime as it caused too many problems with the database

--- datamodel/src/main/java/com/yfy/beem/serverv2/datamodel/User.java
+++ datamodel/src/main/java/com/yfy/beem/serverv2/datamodel/User.java
@@ -2,6 +2,7 @@ package com.yfy.beem.serverv2.datamodel;
 
 import lombok.*;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -20,9 +21,13 @@ import java.time.LocalDateTime;
 @Builder
 public class User {
     @Id
+    @Column(name="id",nullable = false, updatable = false)
     private Long id;
+    @Column(name = "name", nullable = false)
     private String name;
+    @Column(name = "key", nullable = false)
     private String publicKey;
+    @Column(name = "ip", nullable = false)
     private String ipAddress;
-    private LocalDateTime savedDate;
+
 }
