90cf67b35ee08126a673dfd983dc6e78f4097c87
teppah <yifengyang01@gmail.com>
2018-12-26 10:43:04 -0500

    created mappings constants class

---
+++ api/src/main/java/com/yfy/beem/serverv2/util/Mappings.java
@@ -0,0 +1,14 @@
+package com.yfy.beem.serverv2.util;
+
+/**
+ * Class that holds mappings for REST endpoints.
+ * */
+public final class Mappings {
+    // do not let anyone instantiate this class
+    private Mappings() {
+        throw new UnsupportedOperationException("dude what are you doing");
+    }
+
+    public static final String API_ROOT = "api";
+    public static final String GET_USERS = "users";
+}
