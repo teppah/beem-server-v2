ed6fbfbf5cc6c4fdd3b145073e63bf8bdffe4048
teppah <yifengyang01@gmail.com>
2018-12-26 21:57:38 -0500

    added some logging

---
+++ api/src/main/java/com/yfy/beem/serverv2/interceptor/RequestInterceptor.java
@@ -0,0 +1,18 @@
+package com.yfy.beem.serverv2.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class RequestInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return true;
+    }
+
+
+}
