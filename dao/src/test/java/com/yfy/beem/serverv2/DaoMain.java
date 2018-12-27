5fd6cadd766e6549b90577ebc470c69751748c83
teppah <yifengyang01@gmail.com>
2018-12-26 19:04:55 -0500

    finished implementing tests for UserDao

---
+++ dao/src/test/java/com/yfy/beem/serverv2/DaoMain.java
@@ -0,0 +1,11 @@
+package com.yfy.beem.serverv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaoMain {
+    public static void main(String[] args) {
+        SpringApplication.run(DaoMain.class);
+    }
+}
